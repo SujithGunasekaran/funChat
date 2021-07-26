e078f95f2ee1de1841830d69fd2278e017eacceb
SujithGunasekaran <mydevelopstar@gmail.com>
2021-07-26 13:06:46 +0530

    roomReducer removed

--- funchat/src/reduxStore/Reducer/index.js
+++ funchat/src/reduxStore/Reducer/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
-import roomReducer from './roomReducer';
 
 export default combineReducers({
-    userReducer,
-    roomReducer
+    userReducer
 })
--- funchat/src/reduxStore/Types/index.js
+++ funchat/src/reduxStore/Types/index.js
@@ -1,4 +1,2 @@
 export const SET_USER_LOGGED_IN = 'SET_USER_LOGGED_IN';
 export const SET_USER_INFO = 'SET_USER_INFO';
-
-export const SET_ROOM_INFO = 'SET_ROOM_INFO';
